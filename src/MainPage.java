f3d841d1994569bb314363b59787d394e65478cb
AbdYildiz <abdullah-yildiz61@hotmail.com>
2022-08-29 10:48:48 +0300

    Mobile and MainPage modified

--- src/MainPage.java
+++ src/MainPage.java
@@ -1,98 +1,144 @@
-import org.checkerframework.checker.units.qual.A;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
+import org.testng.asserts.SoftAssert;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.Duration;
+import java.util.List;
 
 public class MainPage {
     public WebDriver driver = new ChromeDriver();
+    public Actions actions = new Actions(driver);
 
-    @BeforeClass public void maximize(){
-        driver.manage().window().maximize();
-    }
-
-    @BeforeMethod public void getSite(){
+    @BeforeMethod (groups = {"header" , "search", "others"}) public void toMain(){
         driver.get("https://abs.firat.edu.tr/tr");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
 
-    @Test public void emptySearchBox(){
-        driver.findElement(By.id("search")).sendKeys(Keys.ENTER);
-        Assert.assertEquals(driver.getCurrentUrl(),"https://abs.firat.edu.tr/tr?arast%C4%B1rma=");
+    @Test (groups = { "header"}) public void hizliErisim_obs(){
+        actions.moveToElement(driver.findElement(By.xpath("//div[@class='top-right-sub-list']//a[contains(text(),'Hızlı Eri')]"))).perform();
+        driver.findElement(By.xpath("//div[@class='top-right-sub-list']//ul[1]//li[1]")).click();
+        Assert.assertEquals(driver.getCurrentUrl(),"https://obs.firat.edu.tr/");
     }
 
-    @Test public void invalidSearchChar(){
-        String temp = ".?-*8";
-        driver.findElement(By.id("search")).sendKeys(temp);
-        driver.findElement(By.id("search")).sendKeys(Keys.ENTER);
-        Assert.assertEquals(driver.getCurrentUrl(),"https://abs.firat.edu.tr/tr?arast%C4%B1rma=.%3F-*8");
+    @Test (groups = { "header"}) public void hizliErisim_pbs(){
+        actions.moveToElement(driver.findElement(By.xpath("//div[@class='top-right-sub-list']//a[contains(text(),'Hızlı Eri')]"))).perform();
+        driver.findElement(By.xpath("//div[@class='top-right-sub-list']//ul[1]//li[2]")).click();
+        Assert.assertEquals(driver.getCurrentUrl(),"https://pbs.firat.edu.tr/");
     }
 
-    @Test public void isDropDownVisible(){
-        driver.findElement(By.id("search")).sendKeys("mustafa");
-        driver.findElement(By.id("search")).clear();
-        driver.findElement(By.id("search")).click();
-        boolean dropDown = driver.findElement(By.xpath("//form//a[1]")).isDisplayed();
-        Assert.assertFalse(dropDown);
+    @Test (groups = { "header"}) public void hizliErisim_UniMain(){
+        actions.moveToElement(driver.findElement(By.xpath("//div[@class='top-right-sub-list']//a[contains(text(),'Hızlı Eri')]"))).perform();
+        driver.findElement(By.xpath("//div[@class='top-right-sub-list']//ul[1]//li[3]")).click();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.firat.edu.tr/tr");
     }
 
-    @Test public void toLogin(){
-        driver.findElement(By.xpath("//a[contains(text(),'Yap')]")).click();
-        Assert.assertEquals(driver.getCurrentUrl(), "https://jasig.firat.edu.tr/cas/login?service=https%3A%2F%2Fabs.firat.edu.tr%2Ftr%2Flogin%2Ffirat%2Fcas");
+    @Test (groups = { "header"}) public void hizliErisim_PhoneBook(){
+        actions.moveToElement(driver.findElement(By.xpath("//div[@class='top-right-sub-list']//a[contains(text(),'Hızlı Eri')]"))).perform();
+        driver.findElement(By.xpath("//div[@class='top-right-sub-list']//ul[1]//li[4]")).click();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.firat.edu.tr/tr/contact");
     }
 
-    @Test public void toStatistics(){
-        driver.findElement(By.xpath("//li[contains(text(),'KLER')]")).click();
-        Assert.assertEquals(driver.getCurrentUrl(), "https://abs.firat.edu.tr/tr/statistics");
+    @Test (groups = { "header"}) public void language_tr(){
+        actions.moveToElement(driver.findElement(By.xpath("//div[@class='top-right-sub-list']//a[contains(text(),'Dil')]"))).perform();
+        driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[2]/div[1]/nav[1]/ul[1]/li[2]/div[1]/ul[1]/li[1]/a[1]")).click();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://abs.firat.edu.tr/tr");
     }
 
-    @Test public void toMain(){
+    @Test (groups = { "header"}) public void language_en(){
+        actions.moveToElement(driver.findElement(By.xpath("//div[@class='top-right-sub-list']//a[contains(text(),'Dil')]"))).perform();
+        driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[2]/div[1]/nav[1]/ul[1]/li[2]/div[1]/ul[1]/li[2]/a[1]")).click();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://abs.firat.edu.tr/en");
+    }
+
+    @Test (groups = { "header"}) public void toMainPage(){
         driver.findElement(By.xpath("//li[contains(text(),'ANASAYFA')]")).click();
         Assert.assertEquals(driver.getCurrentUrl(),"https://abs.firat.edu.tr/tr");
     }
 
-    @Test public void toEnglish(){
-        driver.findElement(By.cssSelector("div.header:nth-child(1) div.top-right-menus div.top-right nav:nth-child(1) ul:nth-child(1) li:nth-child(2) div.top-right-sub-list > a:nth-child(1)")).click();
-        driver.findElement(By.cssSelector("div.header:nth-child(1) div.top-right-menus div.top-right li:nth-child(2) div.top-right-sub-list ul:nth-child(3) li:nth-child(2) > a:nth-child(1)")).click();
-        Assert.assertEquals(driver.getCurrentUrl(),"https://abs.firat.edu.tr/en");
+    @Test (groups = {"header"}) public void statistics(){
+        driver.findElement(By.xpath("//li[contains(text(),'STAT')]")).click();
+        Assert.assertEquals(driver.getCurrentUrl(),"https://abs.firat.edu.tr/tr/statistics");
     }
 
-    @Test public void toTurkish(){
-        driver.findElement(By.cssSelector("div.header:nth-child(1) div.top-right-menus div.top-right nav:nth-child(1) ul:nth-child(1) li:nth-child(2) div.top-right-sub-list > a:nth-child(1)")).click();
-        driver.findElement(By.xpath("//div[@class='top-right-sub-list']//a[contains(text(),'Türkçe')]")).click();
+    @Test (groups = {"header"}) public void btn_Login(){
+        driver.findElement(By.xpath("//p[contains(text(),'Yap')]")).click();
+        Assert.assertEquals(driver.getCurrentUrl(),"https://jasig.firat.edu.tr/cas/login?service=https%3A%2F%2Fabs.firat.edu.tr%2Ftr%2Flogin%2Ffirat%2Fcas");
+    }
+
+    @Test (groups = {"search"}) public void searchBox(){
+        driver.findElement(By.id("search")).sendKeys(" ",Keys.ENTER);
         Assert.assertEquals(driver.getCurrentUrl(),"https://abs.firat.edu.tr/tr");
+        driver.findElement(By.id("search")).sendKeys("./_*?",Keys.ENTER);
+        Assert.assertEquals(driver.getCurrentUrl(),"https://abs.firat.edu.tr/tr");
+        driver.findElement(By.id("search")).clear();
+    }
+    @Test (groups = {"search"}) public void searchBox_firstItem(){
+        driver.findElement(By.id("search")).sendKeys("mustafa ulas");
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='userOption']//a[1]")));
+        driver.findElement(By.id("search")).sendKeys(Keys.ARROW_DOWN,Keys.ENTER);
+        Assert.assertEquals(driver.getCurrentUrl(),"https://abs.firat.edu.tr/tr/mustafaulas");
     }
 
-    @Test public void toOBS(){
-        driver.findElement(By.xpath("//div[@class='top-right-sub-list']//a[contains(text(),'renci Bilgi Sistemi')]")).click();
-        Assert.assertEquals(driver.getCurrentUrl(), "https://obs.firat.edu.tr/");
+    @Test (groups = {"search"}) public void searchBox_lastItem() {
+        driver.findElement(By.id("search")).sendKeys("mustafa ulas");
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='userOption']//a[1]")));
+        driver.findElement(By.id("search")).sendKeys(Keys.ARROW_DOWN,Keys.ARROW_DOWN,Keys.ENTER);
+        Assert.assertEquals(driver.getCurrentUrl(),"https://abs.firat.edu.tr/tr/m.ulas");
     }
 
-    @Test public void toPBS(){
-        driver.findElement(By.xpath("//div[@class='top-right-sub-list']//a[contains(text(),'Personel Bilgi Sistemi')]")).click();
-        Assert.assertEquals(driver.getCurrentUrl(), "https://pbs.firat.edu.tr/");
+    @Test (groups = {"others"}) public void lastUpdated() throws IOException {
+        List<WebElement> last = driver.findElements(By.xpath("//div[@class='last-updated col-xl-4 col-md-6']//a"));
+        SoftAssert soft = new SoftAssert();
+
+        for (WebElement a: last) {
+            String url = a.getAttribute("href");
+            HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+            conn.setRequestMethod("HEAD");
+            conn.connect();
+            soft.assertTrue(conn.getResponseCode() < 400,a.getText() + "          BROKEN LINK");
+        }
+        soft.assertAll();
     }
 
-    @Test public void toUniMain(){
-        driver.findElement(By.xpath("//div[@class='top-right-sub-list']//a[contains(text(),'Üniversite Anasayfa')]")).click();
-        Assert.assertEquals(driver.getCurrentUrl(),"https://www.firat.edu.tr/tr");
+    @Test (groups = {"others"}) public void lastAdded() throws IOException {
+        List<WebElement> href = driver.findElements(By.xpath("//div[@class='recently-added-posts col-xl-4 col-md-6']//a"));
+        List<WebElement> text = driver.findElements(By.xpath("//div[@class='recently-added-posts col-xl-4 col-md-6']//h6"));
+        SoftAssert soft = new SoftAssert();
+
+        for (int i = 0; i < href.size(); i++) {
+            String url = href.get(i).getAttribute("href");
+            HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+            conn.setRequestMethod("HEAD");
+            conn.connect();
+            soft.assertTrue(conn.getResponseCode() < 400,text.get(i).getText() + "             BROKEN LINK");
+        }
+        soft.assertAll();
     }
 
-    @Test public void toPhoneBook(){
-        driver.findElement(By.xpath("//div[@class='top-right-sub-list']//a[contains(text(),'Telefon Rehberi')]")).click();
-        Assert.assertEquals(driver.getCurrentUrl(), "https://www.firat.edu.tr/tr/contact");
+    @Test (groups = {"others"}) public void email_ddyo(){
+        String isit = driver.findElement(By.xpath("//p[contains(text(),'ddyo@firat.edu.tr')]")).getAttribute("href");
+        Assert.assertNotNull(isit);
     }
 
-    @Test public void toLastUpdated(){
-        driver.findElement(By.xpath("//div[@class='part2 container']//a[1]//div[1]")).click();
-        Assert.assertEquals(driver.getCurrentUrl(),"https://abs.firat.edu.tr/tr/askinsen");
+    @Test(groups = {"others"}) public void footer_ddyo(){
+        driver.findElement(By.linkText("Dijital Dönüşüm ve Yazılım Ofisi")).click();
+        Assert.assertEquals(driver.getCurrentUrl(), "http://ddyo.firat.edu.tr/tr");
     }
 
-    @AfterClass public void close() throws InterruptedException {
-        Thread.sleep(2000);
+
+    @AfterClass (groups = {"header", "search", "others"}) public void close(){
         driver.quit();
     }
 }
--- src/MobileMainPage.java
+++ src/MobileMainPage.java
@@ -1,103 +1,148 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
+import org.testng.asserts.SoftAssert;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.Duration;
+import java.util.List;
+
 
 public class MobileMainPage {
     public WebDriver driver = new ChromeDriver();
 
-    @BeforeMethod public void getSite(){
-        driver.get("https://abs.firat.edu.tr/tr");
+    @BeforeClass (groups = {"mobileBtn", "search", "others"}) public void newDimensions(){
         driver.manage().window().setSize(new Dimension(375,667));
+        driver.get("https://abs.firat.edu.tr/tr");
     }
 
-    @Test public void emptySearchBox(){
-        driver.findElement(By.id("search")).sendKeys(Keys.ENTER);
-        Assert.assertEquals(driver.getCurrentUrl(),"https://abs.firat.edu.tr/tr?arast%C4%B1rma=");
+    @BeforeMethod (groups = {"mobileBtn" , "search", "others"}) public void toMain(){
+        driver.get("https://abs.firat.edu.tr/tr");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
 
-    @Test public void invalidSearchChar(){
-        String temp = ".?-*8";
-        driver.findElement(By.id("search")).sendKeys(temp);
-        driver.findElement(By.id("search")).sendKeys(Keys.ENTER);
-        Assert.assertEquals(driver.getCurrentUrl(),"https://abs.firat.edu.tr/tr?arast%C4%B1rma=.%3F-*8");
+    @Test (groups = { "mobileBtn"}) public void mobileBtn_obs(){
+        driver.findElement(By.xpath("//div[@class='mobil-button']")).click();
+        driver.findElement(By.xpath("//div[@class='mobileMenu']//li[.='Öğrenci Bilgi Sistemi']")).click();
+        Assert.assertEquals(driver.getCurrentUrl(),"https://obs.firat.edu.tr/");
     }
 
-    @Test public void isDropDownVisible(){
-        driver.findElement(By.id("search")).sendKeys("mustafa");
-        driver.findElement(By.id("search")).clear();
-        driver.findElement(By.id("search")).click();
-        boolean dropDown = driver.findElement(By.xpath("//form//a[1]")).isDisplayed();
-        Assert.assertFalse(dropDown);
+    @Test (groups = { "mobileBtn"}) public void mobileBtn_pbs(){
+        driver.findElement(By.xpath("//div[@class='mobil-button']")).click();
+        driver.findElement(By.xpath("//div[@class='mobileMenu']//li[.='Personel Bilgi Sistemi']")).click();
+        Assert.assertEquals(driver.getCurrentUrl(),"https://pbs.firat.edu.tr/");
     }
 
-    @Test public void isMobileButtonVisible(){
-        driver.manage().window().setSize(new Dimension(750,750));
-        driver.navigate().refresh();
-        boolean mb = driver.findElement(By.xpath("//body/div[@class='mobileMenu']/ul/li[1]/a[1]")).isDisplayed();
-        Assert.assertFalse(mb);
+    @Test (groups = { "mobileBtn"}) public void mobileBtn_UniMain(){
+        driver.findElement(By.xpath("//div[@class='mobil-button']")).click();
+        driver.findElement(By.xpath("//div[@class='mobileMenu']//li[.='Üniversite Anasayfa']")).click();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.firat.edu.tr/tr");
     }
 
-    @Test public void toMobileLogin(){
-        driver.findElement(By.xpath("//a[contains(text(),'Yap')]")).click();
-        Assert.assertEquals(driver.getCurrentUrl(), "https://jasig.firat.edu.tr/cas/login?service=https%3A%2F%2Fabs.firat.edu.tr%2Ftr%2Flogin%2Ffirat%2Fcas");
-    }
-
-    @Test public void toMobileStatistics(){
-        driver.findElement(By.xpath("//a[contains(text(),'statistikler')]")).click();
-        Assert.assertEquals(driver.getCurrentUrl(), "https://abs.firat.edu.tr/tr/statistics");
-    }
-
-    @Test public void toMobileEnglish(){
-        driver.findElement(By.xpath("//div[@class='mobileMenu']//a[contains(text(),'English')]")).click();
-        Assert.assertEquals(driver.getCurrentUrl(),"https://abs.firat.edu.tr/en");
-    }
-
-    @Test public void toMobileTurkish(){
-        driver.findElement(By.xpath("//div[@class='mobileMenu']//a[contains(text(),'Türkçe')]")).click();
-        Assert.assertEquals(driver.getCurrentUrl(),"https://abs.firat.edu.tr/tr");
-    }
-
-    @Test public void toMobileOBS(){
-        driver.findElement(By.xpath("//div[@class='mobileMenu']//a[contains(text(),'renci Bilgi Sistemi')]")).click();
-        Assert.assertEquals(driver.getCurrentUrl(), "https://obs.firat.edu.tr/");
-    }
-
-    @Test public void toMobilePBS(){
-        driver.findElement(By.xpath("//div[@class='mobileMenu']//a[contains(text(),'Personel Bilgi Sistemi')]")).click();
-        Assert.assertEquals(driver.getCurrentUrl(), "https://pbs.firat.edu.tr/");
-    }
-
-    @Test public void toMobileUniMain(){
-        driver.findElement(By.xpath("//div[@class='mobileMenu']//a[contains(text(),'Üniversite Anasayfa')]")).click();
-        Assert.assertEquals(driver.getCurrentUrl(),"https://www.firat.edu.tr/tr");
-    }
-
-    @Test public void toMobilePhoneBook(){
-        driver.findElement(By.xpath("//div[@class='mobileMenu']//a[contains(text(),'Telefon Rehberi')]")).click();
+    @Test (groups = { "mobileBtn"}) public void mobileBtn_PhoneBook(){
+        driver.findElement(By.xpath("//div[@class='mobil-button']")).click();
+        driver.findElement(By.xpath("//div[@class='mobileMenu']//li[.='Telefon Rehberi']")).click();
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.firat.edu.tr/tr/contact");
     }
 
-    @Test public void MobileBtOpen(){
-        driver.findElement(By.xpath("//button[@class='navbar-toggler collapsed close']")).click();
-        boolean mb = driver.findElement(By.xpath("//body/div[@class='mobileMenu']/ul/li[1]/a[1]")).isDisplayed();
-        Assert.assertTrue(mb);
-    }
-
-    @Test public void MobileBtClose() throws InterruptedException {
-        driver.findElement(By.xpath("//button[@class='navbar-toggler collapsed close']")).click();
+    @Test (groups = { "mobileBtn"}) public void mobileBtn_language_tr(){
         driver.findElement(By.xpath("//div[@class='mobil-button']")).click();
-        Thread.sleep(1000);
-        boolean mb = driver.findElement(By.xpath("//body/div[@class='mobileMenu']/ul/li[1]/a[1]")).isDisplayed();
-        Assert.assertFalse(mb);
+        driver.findElement(By.xpath("//div[@class='mobileMenu']//li[.='Türkçe']")).click();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://abs.firat.edu.tr/tr");
     }
 
-    @AfterClass public void close() throws InterruptedException {
-        Thread.sleep(3000);
+    @Test (groups = { "mobileBtn"}) public void mobileBtn_language_en() {
+        driver.findElement(By.xpath("//div[@class='mobil-button']")).click();
+        driver.findElement(By.xpath("//div[@class='mobileMenu']//a[.='English']")).click();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://abs.firat.edu.tr/en");
+    }
+
+    @Test (groups = { "mobileBtn"}) public void mobileBtn_toMainPage(){
+        driver.findElement(By.xpath("//div[@class='mobil-button']")).click();
+        driver.findElement(By.xpath("//div[@class='mobileMenu']//li[.='Anasayfa']")).click();
+        Assert.assertEquals(driver.getCurrentUrl(),"https://abs.firat.edu.tr/tr");
+    }
+
+    @Test (groups = {"mobileBtn"}) public void mobileBtn_statistics(){
+        driver.findElement(By.xpath("//div[@class='mobil-button']")).click();
+        driver.findElement(By.xpath("//a[contains(text(),'stat')]")).click();
+        Assert.assertEquals(driver.getCurrentUrl(),"https://abs.firat.edu.tr/tr/statistics");
+    }
+
+    @Test (groups = {"mobileBtn"}) public void mobileBtn_btn_Login(){
+        driver.findElement(By.xpath("//div[@class='mobil-button']")).click();
+        driver.findElement(By.xpath("//a[contains(text(),'Yap')]")).click();
+        Assert.assertEquals(driver.getCurrentUrl(),"https://jasig.firat.edu.tr/cas/login?service=https%3A%2F%2Fabs.firat.edu.tr%2Ftr%2Flogin%2Ffirat%2Fcas");
+    }
+
+    @Test (groups = {"search"}) public void searchBox(){
+        driver.findElement(By.id("search")).sendKeys(" ",Keys.ENTER);
+        Assert.assertEquals(driver.getCurrentUrl(),"https://abs.firat.edu.tr/tr");
+        driver.findElement(By.id("search")).sendKeys("./_*?",Keys.ENTER);
+        Assert.assertEquals(driver.getCurrentUrl(),"https://abs.firat.edu.tr/tr");
+        driver.findElement(By.id("search")).clear();
+    }
+    @Test (groups = {"search"}) public void searchBox_firstItem(){
+        driver.findElement(By.id("search")).sendKeys("mustafa ulas");
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='userOption']//a[1]")));
+        driver.findElement(By.id("search")).sendKeys(Keys.ARROW_DOWN,Keys.ENTER);
+        Assert.assertEquals(driver.getCurrentUrl(),"https://abs.firat.edu.tr/tr/mustafaulas");
+    }
+
+    @Test (groups = {"search"}) public void searchBox_lastItem() {
+        driver.findElement(By.id("search")).sendKeys("mustafa ulas");
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='userOption']//a[1]")));
+        driver.findElement(By.id("search")).sendKeys(Keys.ARROW_DOWN,Keys.ARROW_DOWN,Keys.ENTER);
+        Assert.assertEquals(driver.getCurrentUrl(),"https://abs.firat.edu.tr/tr/m.ulas");
+    }
+
+    @Test (groups = {"others"}) public void lastUpdated() throws IOException {
+        List<WebElement> last = driver.findElements(By.xpath("//div[@class='last-updated col-xl-4 col-md-6']//a"));
+        SoftAssert soft = new SoftAssert();
+
+        for (WebElement a: last) {
+            String url = a.getAttribute("href");
+            HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+            conn.setRequestMethod("HEAD");
+            conn.connect();
+            soft.assertTrue(conn.getResponseCode() < 400,a.getText() + "          BROKEN LINK");
+        }
+        soft.assertAll();
+    }
+
+    @Test (groups = {"others"}) public void lastAdded() throws IOException {
+        List<WebElement> href = driver.findElements(By.xpath("//div[@class='recently-added-posts col-xl-4 col-md-6']//a"));
+        List<WebElement> text = driver.findElements(By.xpath("//div[@class='recently-added-posts col-xl-4 col-md-6']//h6"));
+        SoftAssert soft = new SoftAssert();
+
+        for (int i = 0; i < href.size(); i++) {
+            String url = href.get(i).getAttribute("href");
+            HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+            conn.setRequestMethod("HEAD");
+            conn.connect();
+            soft.assertTrue(conn.getResponseCode() < 400,text.get(i).getText() + "             BROKEN LINK");
+        }
+        soft.assertAll();
+    }
+
+    @Test (groups = {"others"}) public void email_ddyo(){
+        String isit = driver.findElement(By.xpath("//p[contains(text(),'ddyo@firat.edu.tr')]")).getAttribute("href");
+        Assert.assertNotNull(isit);
+    }
+
+    @Test(groups = {"others"}) public void footer_ddyo(){
+        driver.findElement(By.linkText("Dijital Dönüşüm ve Yazılım Ofisi")).click();
+        Assert.assertEquals(driver.getCurrentUrl(), "http://ddyo.firat.edu.tr/tr");
+    }
+
+
+    @AfterClass (groups = {"mobileBtn", "search", "others"}) public void close(){
         driver.quit();
     }
-
 }
